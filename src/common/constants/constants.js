e196e0f6d268402c73cae9efcf28a7d1efed416b
yneskladovae <yneskladovae27@gmail.com>
2023-07-08 00:18:37 +0300

    feat: stylized custom components and Schedule.jsx, add constants, assets

---
+++ src/common/constants/constants.js
@@ -0,0 +1,71 @@
+export const WEEK_DAYS = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
+export const MON_WED_FRI = ["ПН", "СР", "ПТ"];
+export const TUE_THU = ["ВТ", "ЧТ"];
+export const timeSelectionOptions = [
+  {
+    label: "Астрономическиe",
+    value: "astronomical",
+  },
+  {
+    label: "Академическиe",
+    value: "academical",
+  },
+];
+export const breakSelectionOptions = [
+  {
+    label: "Без перерыва",
+    value: 0,
+  },
+  {
+    label: "5 мин",
+    value: 5,
+  },
+  {
+    label: "10 мин",
+    value: 10,
+  },
+  {
+    label: "20 мин",
+    value: 20,
+  },
+  {
+    label: "30 мин",
+    value: 30,
+  },
+];
+export const teacherSelectionOptions = [
+  {
+    label: "Выберите преподавателя на это время",
+    value: null,
+  },
+  {
+    label: "Ivanov",
+    value: "Ivanov",
+  },
+  {
+    label: "Petrov",
+    value: "Petrov",
+  },
+  {
+    label: "Sidarov",
+    value: "Sidarov",
+  },
+];
+export const classroomSelectionOptions = [
+  {
+    label: "Аудитория",
+    value: null,
+  },
+  {
+    label: "№1",
+    value: 1,
+  },
+  {
+    label: "№2",
+    value: 2,
+  },
+  {
+    label: "№3",
+    value: 3,
+  },
+];
--- src/features/Schedule/Schedule.jsx
+++ src/features/Schedule/Schedule.jsx
@@ -1,82 +1,23 @@
 import React from "react";
+import ScheduleSettings from "../../common/styles/ScheduleSettings.module.css";
 import styles from "./Schedule.module.css";
 import { Input } from "../../components/Input/Input";
 import { Select } from "../../components/Select/Select";
 import { Button } from "../../components/Button/Button";
+import {
+  breakSelectionOptions,
+  classroomSelectionOptions,
+  teacherSelectionOptions,
+  timeSelectionOptions,
+  WEEK_DAYS,
+} from "../../common/constants/constants";
 
-const timeSelectionOptions = [
-  {
-    label: "Астрономическиe",
-    value: "astronomical",
-  },
-  {
-    label: "Академическиe",
-    value: "academical",
-  },
-];
-
-export const breakSelectionOptions = [
-  {
-    label: "Без перерыва",
-    value: 0,
-  },
-  {
-    label: "5 мин",
-    value: 5,
-  },
-  {
-    label: "10 мин",
-    value: 10,
-  },
-  {
-    label: "20 мин",
-    value: 20,
-  },
-  {
-    label: "30 мин",
-    value: 30,
-  },
-];
-export const teacherSelectionOptions = [
-  {
-    label: "Выберите преподавателя на это время",
-    value: null,
-  },
-  {
-    label: "Ivanov",
-    value: "Ivanov",
-  },
-  {
-    label: "Petrov",
-    value: "Petrov",
-  },
-  {
-    label: "Sidarov",
-    value: "Sidarov",
-  },
-];
-export const classroomSelectionOptions = [
-  {
-    label: "Аудитория",
-    value: null,
-  },
-  {
-    label: "№1",
-    value: 1,
-  },
-  {
-    label: "№2",
-    value: 2,
-  },
-  {
-    label: "№3",
-    value: 3,
-  },
-];
 export const Schedule = () => {
   return (
     <div className={styles.scheduleContainer}>
-      <div className={styles.schoolNameAndColorBlock}>
+      <div
+        className={`${ScheduleSettings.settingsBlock} ${styles.schoolNameAndColorBlock}`}
+      >
         <Input
           type="text"
           placeholder={'Школа "Мамыр"'}
@@ -93,11 +34,11 @@ export const Schedule = () => {
           </label>
         </div>
       </div>
-      <div>
-        <div>
+      <div className={ScheduleSettings.settingsBlock}>
+        <div className={ScheduleSettings.settingsBlockItem}>
           <Select options={timeSelectionOptions} />
         </div>
-        <div>
+        <div className={ScheduleSettings.settingsBlockItem}>
           <div className={styles.setValueBlock}>
             <Button classNameValue={styles.btnMinus}>-</Button>
             <div className={styles.valueScreen}>
@@ -107,44 +48,46 @@ export const Schedule = () => {
             <Button classNameValue={styles.btnPlus}>+</Button>
           </div>
         </div>
-        <div>
+        <div className={ScheduleSettings.settingsBlockItem}>
           <Input type="date" classNameValue={styles.startData} />
           <span className={styles.separator}>до</span>
           <Input type="date" classNameValue={styles.endData} />
         </div>
       </div>
-      <div className={styles.daysOfWeekBlock}>
+      <div
+        className={`${ScheduleSettings.settingsBlock} ${styles.daysOfWeekBlock}`}
+      >
         <Button classNameValue={styles.weekDayBtn}>ПН/СР/ПТ</Button>
         <Button classNameValue={styles.weekDayBtn}>ВТ/ЧТ</Button>
-        <Button classNameValue={styles.weekDayBtn}>ПН</Button>
-        <Button classNameValue={styles.weekDayBtn}>ВТ</Button>
-        <Button classNameValue={styles.weekDayBtn}>СР</Button>
-        <Button classNameValue={styles.weekDayBtn}>ЧТ</Button>
-        <Button classNameValue={styles.weekDayBtn}>ПН</Button>
-        <Button classNameValue={styles.weekDayBtn}>СБ</Button>
-        <Button classNameValue={styles.weekDayBtn}>ВС</Button>
+        {WEEK_DAYS.map((day) => (
+          <Button key={day} classNameValue={styles.weekDayBtn}>
+            {day}
+          </Button>
+        ))}
       </div>
-      <div>
-        <div>
+      <div className={ScheduleSettings.settingsBlock}>
+        <div className={ScheduleSettings.settingsBlockItem}>
           <Select options={breakSelectionOptions} />
         </div>
-        <div>
+        <div className={ScheduleSettings.settingsBlockItem}>
           <div className={styles.setValueBlock}>
             <Button classNameValue={styles.btnMinus}>-</Button>
             <div className={styles.valueScreen}>
-              <div className={styles.value}>3</div>
-              <div className={styles.valueDescription}>Всего часов</div>
+              <div className={styles.value}>1</div>
+              <div className={styles.valueDescription}>Часов в день</div>
             </div>
             <Button classNameValue={styles.btnPlus}>+</Button>
           </div>
         </div>
-        <div>
-          <Input type="date" classNameValue={styles.startData} />
+        <div className={ScheduleSettings.settingsBlockItem}>
+          <Input type="time" classNameValue={styles.startData} />
           <span className={styles.separator}>до</span>
-          <Input type="date" classNameValue={styles.endData} />
+          <Input type="time" classNameValue={styles.endData} />
         </div>
       </div>
-      <div className={styles.teacherAndClassroomBlock}>
+      <div
+        className={`${ScheduleSettings.settingsBlock} ${styles.teacherAndClassroomBlock}`}
+      >
         <div className={styles.selectTeacher}>
           <Select options={teacherSelectionOptions} />
         </div>
