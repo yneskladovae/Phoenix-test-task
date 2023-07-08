a9cd41c92dc7cb37db7d9b0c50f9ce2ecf146eba
yneskladovae <yneskladovae27@gmail.com>
2023-07-08 21:40:11 +0300

    feat: implement calculateEndCourseDate func, add useEffect

--- src/features/Schedule/Schedule.jsx
+++ src/features/Schedule/Schedule.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ScheduleSettings from "../../common/styles/ScheduleSettings.module.css";
 import styles from "./Schedule.module.css";
 import { Input } from "../../components/Input/Input";
@@ -32,6 +32,33 @@ export const Schedule = () => {
   const [endCourseDate, setEndCourseDate] = useState("");
   const lessonStartFormatTime = formatTime(lessonStart);
   const lessonEndFormatTime = formatTime(lessonEnd);
+
+  const calculateEndCourseDate = (startCourseDate, totalCourseTime, hoursPerDay, dayOfLessons) => {
+    const startDate = new Date(startCourseDate);
+    const endDate = new Date(startDate);
+    const daysOf = ["ВС", "ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ"];
+    const daysNeeded = Math.ceil(totalCourseTime / hoursPerDay);
+    let lessonCount = 0;
+
+    while (lessonCount < daysNeeded) {
+      if (dayOfLessons.length === 0) {
+        return;
+      }
+      endDate.setDate(endDate.getDate() + 1);
+      const currentDay = daysOf[endDate.getDay()];
+
+      if (dayOfLessons.includes(currentDay)) {
+        lessonCount++;
+      }
+    }
+
+    return endDate.toLocaleDateString("en-CA");
+  };
+
+  useEffect(() => {
+    const endDate = calculateEndCourseDate(startCourseDate, totalCourseTime, hoursPerDay, dayOfLessons);
+    setEndCourseDate(endDate);
+  }, [startCourseDate, totalCourseTime, hoursPerDay, dayOfLessons]);
 
   const changeTypeTimeHandler = (e) => {
     const currTypeTimeValue = e.currentTarget.value;
@@ -122,7 +149,7 @@ export const Schedule = () => {
             classNameValue={styles.startData}
           />
           <span className={styles.separator}>до</span>
-          <Input type="date" classNameValue={styles.endData} readOnly />
+          <Input type="date" value={endCourseDate} classNameValue={styles.endData} readOnly />
         </div>
       </div>
       <div className={`${ScheduleSettings.settingsBlock} ${styles.daysOfWeekBlock}`}>
